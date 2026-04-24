13f38288c3bac820a45b23232824f2061515b663
Hassan <Hassan@DESKTOP-LJOV3TT>
2026-04-24 23:22:04 +0300

    Refactor attempt/answer queries and scoring logic
    Added MediatR query handlers for attempts, answers, and correct options. Refactored GetAttemptTimerQueryHandler to use new queries, improving modularity and maintainability. Updated .csproj to organize new feature folders.

---
+++ Features/Attempts/Queries/GetAttemptByIdQuery.cs
@@ -0,0 +1,29 @@
+﻿using Examination_System.Common.Models;
+using Examination_System.Common.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examination_System.Features.Attempts.Queries
+{
+    public record GetAttemptByIdQuery(Guid AttemptId) : IRequest<Attempt>;
+
+    public class GetAttemptByIdQueryHandler : IRequestHandler<GetAttemptByIdQuery, Attempt>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetAttemptByIdQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Attempt> Handle(GetAttemptByIdQuery request, CancellationToken cancellationToken)
+        {
+            var attemptRepository = _unitOfWork.Repository<Attempt>();
+            var attempt = await attemptRepository.GetAll()
+                .Where(x => x.Id == request.AttemptId)
+                .FirstOrDefaultAsync();
+            return attempt;
+        }
+    }
+
+
+
+}
---
+++ Features/QuestionOptions/Queries/GetAllCorrectOptions.cs
@@ -0,0 +1,28 @@
+﻿using Examination_System.Common.Models;
+using Examination_System.Common.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examination_System.Features.QuestionOptions.Queries
+{
+    public record GetAllCorrectOptions() : IRequest<IEnumerable<Option>>;
+
+    public class GetAllCorrectOptionsHandler : IRequestHandler<GetAllCorrectOptions, IEnumerable<Option>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetAllCorrectOptionsHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<Option>> Handle(GetAllCorrectOptions request, CancellationToken cancellationToken)
+        {
+            var optionRepository = _unitOfWork.Repository<Option>();
+            var correctOptions = await optionRepository.GetAll()
+                .Where(o => o.IsCorrect)
+                .ToListAsync();
+
+            return correctOptions;
+        }
+
+    }
+}
