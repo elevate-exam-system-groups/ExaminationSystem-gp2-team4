b1ccb5447c177cb7ae06f427bea6ce0b730f7ee3
Hassan <Hassan@DESKTOP-LJOV3TT>
2026-04-25 08:32:41 +0300

    Add admin endpoints and DTOs for attempt listing/details
    Added DTOs and queries for paginated attempt listing and detailed attempt view. Updated AdminController with new endpoints. Removed Answers collection from Attempt model. Added caching for attempt queries.

---
+++ Features/Admin/DTOs/GetAllAttemptsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Examination_System.Features.Admin.DTOs
+{
+    public class GetAllAttemptsDTO
+    {
+        public List<AttemptDTO> Attempts { get; set; }
+        public int PageNum { get; set; }   
+        public int ItemsPerPage { get; set; }
+        public int TotalCount { get; set; } 
+
+    }
+}
---
+++ Features/Admin/Queries/GetAttemptDetailById.cs
@@ -0,0 +1,45 @@
+﻿using Examination_System.Common.Models;
+using Examination_System.Common.Repositories;
+using Examination_System.Common.Wrappers;
+using Examination_System.Features.Admin.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examination_System.Features.Admin.Queries
+{
+    public record GetAttemptDetailById(Guid AttemptId) : IRequest<ApiResponse<AttemptDetailDTO>>;
+    
+    public class GetAttemptDetailByIdHandler : IRequestHandler<GetAttemptDetailById, ApiResponse<AttemptDetailDTO>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetAttemptDetailByIdHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ApiResponse<AttemptDetailDTO>> Handle(GetAttemptDetailById request, CancellationToken cancellationToken)
+        {
+            var AttemptRepository = _unitOfWork.Repository<Attempt>();
+            var attempt =  AttemptRepository.Find(a => a.Id == request.AttemptId)
+                .Select(a=>new AttemptDetailDTO()
+                {
+                    UserId = a.UserId,
+                    QuizId = a.QuizId,
+                    Status = a.Status,
+                    StartTime = a.StartTime,
+                    SubmittedAt = a.SubmittedAt,
+                    TotalQuestions = a.Quiz.Questions.Count(),
+                    Score = a.Score,
+                    IsPassed = a.IsPassed,
+                    QuestionsData = a.Quiz.Questions.Select( q => new AttemptQuestionDataDTO
+                    {
+                        QuestionText = q.Body,
+                        AnswerText =  a.Answers.Select(an=>an.Option.Body).FirstOrDefault(),        
+                        IsCorrect = a.Answers.Select(an => an.Option.IsCorrect).FirstOrDefault()
+                    }).ToList()
+                }).FirstOrDefault();
+
+            return ApiResponse<AttemptDetailDTO>.Success(attempt);
+        }
+    }
+
+}
